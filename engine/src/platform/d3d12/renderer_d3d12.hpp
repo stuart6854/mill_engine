a0454a4b15ec05dc29b940b23698bc8119227e17
Stuart Millman <stu.millman15@gmail.com>
2023-03-10 14:06:14 +0000

    Removed unused D3D12 code

--- engine/src/platform/d3d12/renderer_d3d12.hpp
+++ engine/src/platform/d3d12/renderer_d3d12.hpp
@@ -26,27 +26,5 @@ namespace mill::platform
         Owned<DeviceD3D12> m_device{ nullptr };
         Owned<ContextD3D12> m_graphicsContext{ nullptr };
         HWND m_hwnd{ nullptr };
-#if 0
-        static const u32 FrameCount = 2;
-        //IDXGIFactory7* m_factory = nullptr;
-        //ID3D12Device10* m_device = nullptr;
-        DWORD m_debugCallbackCookie{};
-        // Owned<CommandD3D12> m_cmd = nullptr;
-
-        ID3D12CommandQueue* m_cmdQueue = nullptr;
-        ID3D12CommandAllocator* m_cmdAllocator = nullptr;
-        ID3D12GraphicsCommandList* m_cmdList = nullptr;
-
-        ID3D12Fence1* m_fence = nullptr;
-        u64 m_fenceValue = 0;
-        HANDLE m_fenceEvent = nullptr;
-
-        ID3D12DescriptorHeap* m_rtvHeap = nullptr;
-        u32 m_rtvDescriptorSize = 0;
-
-        ComPtr<IDXGISwapChain4> m_swapchain = nullptr;
-        ID3D12Resource* m_renderTargets[FrameCount];
-        u32 m_backbufferIndex = 0;
-#endif
     };
 }
