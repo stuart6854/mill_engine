845a468daf5dc2690345b7f43cb2f635f82892a1
Stuart Millman <stu.millman15@gmail.com>
2023-04-19 10:03:10 +0100

    SceneManager - Logging.

--- engine/src/mill/scene/scene_manager.cpp
+++ engine/src/mill/scene/scene_manager.cpp
@@ -8,11 +8,13 @@ namespace mill
 
     void SceneManager::initialise()
     {
+        LOG_INFO("SceneManager - Initialising...");
         load_scene();
     }
 
     void SceneManager::shutdown()
     {
+        LOG_INFO("SceneManager - Shutting down...");
         m_activeScene->on_unload();
         m_activeScene = nullptr;
     }
@@ -28,11 +30,13 @@ namespace mill
     {
         if (m_activeScene)
         {
+            LOG_INFO("SceneManage - Unloading scene.");
             m_activeScene->on_unload();
             m_activeScene = nullptr;
         }
 
         m_activeScene = CreateShared<Scene>();
+        LOG_INFO("SceneManager - Loading scene.");
         m_activeScene->on_load();
     }
 
